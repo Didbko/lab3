22e7e07e406b044536c1306bd307d642b292b45c
Stepan Belei <102958170+Didbko@users.noreply.github.com>
2023-04-30 16:20:38 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,9 +5,8 @@ for i in "${arr[@]}"; do
 for i in {1..3}; do
 name1=$(date +%M)
 name2=$(date +%S)
-icpc -O$1 -m$j app.cpp -o go-$name1-$name2
+icpc -std=c++17 -O$i -m$j app.cpp -o go-$name1-$name2
 echo "$j $i:"
 time ./go-$name1-$name2
 done
 done 
-`
