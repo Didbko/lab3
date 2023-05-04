19a381f5f6cc8b3c33cda8d8c7508db6b852116a
Stepan Belei <102958170+Didbko@users.noreply.github.com>
2023-05-04 23:47:36 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    for (int i = 0; i < 1000000; i++) {
+        int arr[150];
+        for (int j = 0; j < 150; j++) {
+            arr[j] = rand() % 150 + 1;
+        }
+        int n = sizeof(arr) / sizeof(arr[0]);
+        for (int j = 0; j < 150; j++) {
+            insertionSort(arr, n);
+        }
+    }
+    cout << "Done" << endl;
+    return 0;
+}
