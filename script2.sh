1495452d0ee4c6a5be08ccd7fafda10b591fb05d
Stepan Belei <102958170+Didbko@users.noreply.github.com>
2023-05-04 19:52:30 +0300

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cat /proc/cpuinfo | grep flag | head -1
+cd /home/grid/testbed/tb188/lab 
+ml icc
+arr=("-O1" "-O2" "-O3" "-xSSE2" "-xSSE3" "-xAVX"  )
+for j in "${arr[@]}"; do
+  for i in {1..3}; do
+    x1=$(date +%M)
+    x2=$(date +%S)
+    echo icpc -std=c++17 $j ConsoleApplication1.cpp -o log-$x1-$x2
+    icpc -std=c++17 $j ConsoleApplication1.cpp -o log-$x1-$x2
+    echo "$j $i:"
+    time ./log-$x1-$x2
+  done 
+done
